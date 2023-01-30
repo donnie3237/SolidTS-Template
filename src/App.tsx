fedffbff7d5b380c13e3587dd43e2391134259a8
pardorn3237 <paradorn3237@gmail.com>
2023-01-30 09:36:50 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import type { Component } from 'solid-js';
+import './Modules/App.scss';
+import {Route ,Routes} from 'soli'
+
+const App: Component = () => {
+  return (
+    <div class="App">
+      <h1>hello</h1>
+    </div>
+  );
+};
+
+export default App;
